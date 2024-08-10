1ab778ae5d871b83db681ef9131817aea3036b96
Kevin-San16 <kevindsantos2012@gmail.com>
2024-08-09 21:43:00 -0600

    Entrega final

--- ClientesRepartoApp/Form1.Designer.cs
+++ ClientesRepartoApp/Form1.Designer.cs
@@ -34,12 +34,15 @@ namespace ClientesRepartoApp
             this.lblLogin = new System.Windows.Forms.Label();
             this.btnIniciarSesion = new System.Windows.Forms.Button();
             this.lblCorreo = new System.Windows.Forms.Label();
-            this.lnklblContrasena = new System.Windows.Forms.LinkLabel();
             this.txtCorreo = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtContrasena = new System.Windows.Forms.Label();
             this.btnRegistro = new System.Windows.Forms.Button();
+            this.pbInicioSesion = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInicioSesion)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -49,98 +52,142 @@ namespace ClientesRepartoApp
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(145)))), ((int)(((byte)(104)))));
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(320, 62);
+            this.panel1.Size = new System.Drawing.Size(648, 47);
             this.panel1.TabIndex = 0;
             // 
             // lblLogin
             // 
             this.lblLogin.AutoSize = true;
             this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogin.Location = new System.Drawing.Point(119, 21);
+            this.lblLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lblLogin.Location = new System.Drawing.Point(16, 11);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(62, 24);
+            this.lblLogin.Size = new System.Drawing.Size(337, 29);
             this.lblLogin.TabIndex = 0;
-            this.lblLogin.Text = "Login";
+            this.lblLogin.Text = "CRA - Clientes Reparto App";
             // 
             // btnIniciarSesion
             // 
-            this.btnIniciarSesion.Location = new System.Drawing.Point(185, 256);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(524, 263);
+            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(86, 23);
-            this.btnIniciarSesion.TabIndex = 1;
+            this.btnIniciarSesion.Size = new System.Drawing.Size(115, 28);
+            this.btnIniciarSesion.TabIndex = 4;
             this.btnIniciarSesion.Text = "Iniciar Sesión";
             this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
             // 
             // lblCorreo
             // 
             this.lblCorreo.AutoSize = true;
-            this.lblCorreo.Location = new System.Drawing.Point(26, 135);
+            this.lblCorreo.Location = new System.Drawing.Point(281, 114);
+            this.lblCorreo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCorreo.Name = "lblCorreo";
-            this.lblCorreo.Size = new System.Drawing.Size(38, 13);
+            this.lblCorreo.Size = new System.Drawing.Size(51, 17);
             this.lblCorreo.TabIndex = 2;
             this.lblCorreo.Text = "Correo";
             // 
-            // lnklblContrasena
-            // 
-            this.lnklblContrasena.AutoSize = true;
-            this.lnklblContrasena.Location = new System.Drawing.Point(26, 235);
-            this.lnklblContrasena.Name = "lnklblContrasena";
-            this.lnklblContrasena.Size = new System.Drawing.Size(119, 13);
-            this.lnklblContrasena.TabIndex = 3;
-            this.lnklblContrasena.TabStop = true;
-            this.lnklblContrasena.Text = "¿Olvidó su contraseña?";
-            // 
             // txtCorreo
             // 
-            this.txtCorreo.Location = new System.Drawing.Point(29, 151);
+            this.txtCorreo.Location = new System.Drawing.Point(285, 134);
+            this.txtCorreo.Margin = new System.Windows.Forms.Padding(4);
             this.txtCorreo.Name = "txtCorreo";
-            this.txtCorreo.Size = new System.Drawing.Size(242, 20);
-            this.txtCorreo.TabIndex = 4;
+            this.txtCorreo.Size = new System.Drawing.Size(352, 22);
+            this.txtCorreo.TabIndex = 1;
             // 
-            // textBox1
+            // txtPassword
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 201);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(242, 20);
-            this.textBox1.TabIndex = 6;
+            this.txtPassword.Location = new System.Drawing.Point(285, 196);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(352, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
             // txtContrasena
             // 
             this.txtContrasena.AutoSize = true;
-            this.txtContrasena.Location = new System.Drawing.Point(26, 185);
+            this.txtContrasena.Location = new System.Drawing.Point(281, 176);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtContrasena.Name = "txtContrasena";
-            this.txtContrasena.Size = new System.Drawing.Size(61, 13);
+            this.txtContrasena.Size = new System.Drawing.Size(81, 17);
             this.txtContrasena.TabIndex = 5;
             this.txtContrasena.Text = "Contraseña";
             // 
             // btnRegistro
             // 
-            this.btnRegistro.Location = new System.Drawing.Point(29, 256);
+            this.btnRegistro.Location = new System.Drawing.Point(285, 263);
+            this.btnRegistro.Margin = new System.Windows.Forms.Padding(4);
             this.btnRegistro.Name = "btnRegistro";
-            this.btnRegistro.Size = new System.Drawing.Size(86, 23);
-            this.btnRegistro.TabIndex = 7;
+            this.btnRegistro.Size = new System.Drawing.Size(115, 28);
+            this.btnRegistro.TabIndex = 3;
             this.btnRegistro.Text = "Registrarse";
             this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // pbInicioSesion
+            // 
+            this.pbInicioSesion.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbInicioSesion.BackgroundImage")));
+            this.pbInicioSesion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbInicioSesion.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbInicioSesion.InitialImage")));
+            this.pbInicioSesion.Location = new System.Drawing.Point(5, 50);
+            this.pbInicioSesion.Margin = new System.Windows.Forms.Padding(4);
+            this.pbInicioSesion.Name = "pbInicioSesion";
+            this.pbInicioSesion.Size = new System.Drawing.Size(267, 309);
+            this.pbInicioSesion.TabIndex = 8;
+            this.pbInicioSesion.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DimGray;
+            this.lblTitulo.Location = new System.Drawing.Point(280, 63);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(337, 29);
+            this.lblTitulo.TabIndex = 9;
+            this.lblTitulo.Text = "CRA - Clientes Reparto App";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(524, 310);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(115, 28);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(320, 297);
+            this.ClientSize = new System.Drawing.Size(648, 366);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pbInicioSesion);
             this.Controls.Add(this.btnRegistro);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.txtCorreo);
-            this.Controls.Add(this.lnklblContrasena);
             this.Controls.Add(this.lblCorreo);
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "frmLogin";
-            this.Text = "Form1";
+            this.Text = "CRA - Clientes Reparto App";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInicioSesion)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,11 +199,13 @@ namespace ClientesRepartoApp
         private System.Windows.Forms.Label lblLogin;
         private System.Windows.Forms.Button btnIniciarSesion;
         private System.Windows.Forms.Label lblCorreo;
-        private System.Windows.Forms.LinkLabel lnklblContrasena;
         private System.Windows.Forms.TextBox txtCorreo;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label txtContrasena;
         private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.PictureBox pbInicioSesion;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
